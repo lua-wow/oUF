c7170f455145d901343ba4196531076dad1f2172
pedrozc90 <pedrozc90@gmail.com>
2024-07-28 01:49:55 -0300

    added expansion version variables

--- init.lua
+++ init.lua
@@ -1,3 +1,10 @@
 local _, ns = ...
 ns.oUF = {}
 ns.oUF.Private = {}
+
+-- https://wowpedia.fandom.com/wiki/WOW_PROJECT_ID
+ns.oUF.isRetail = (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE)
+ns.oUF.isClassic = (WOW_PROJECT_ID == WOW_PROJECT_CLASSIC)
+ns.oUF.isTBC = (WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC)
+ns.oUF.isWrath = (WOW_PROJECT_ID == WOW_PROJECT_WRATH_CLASSIC)
+ns.oUF.isCata = (WOW_PROJECT_ID == WOW_PROJECT_CATACLYSM_CLASSIC)
