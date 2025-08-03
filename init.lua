aab3033c3c9d6701984494197574bc91df7301b9
pedrozc90 <pedrozc90@gmail.com>
2025-08-02 22:23:20 -0300

    added mists of pandaria expansion variable

--- init.lua
+++ init.lua
@@ -2,9 +2,10 @@ local _, ns = ...
 ns.oUF = {}
 ns.oUF.Private = {}
 
--- https://wowpedia.fandom.com/wiki/WOW_PROJECT_ID
+-- https://warcraft.wiki.gg/wiki/WOW_PROJECT_ID
 ns.oUF.isRetail = (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE)
 ns.oUF.isClassic = (WOW_PROJECT_ID == WOW_PROJECT_CLASSIC)
 ns.oUF.isTBC = (WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC)
 ns.oUF.isWrath = (WOW_PROJECT_ID == WOW_PROJECT_WRATH_CLASSIC)
 ns.oUF.isCata = (WOW_PROJECT_ID == WOW_PROJECT_CATACLYSM_CLASSIC)
+ns.oUF.isMoP = (WOW_PROJECT_ID == WOW_PROJECT_MISTS_CLASSIC)
